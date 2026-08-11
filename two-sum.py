0946acaf94b2f2b7b472a46adac09967b967f461
zych84103-coder <zych84103@gmail.com>
2026-08-11 15:45:32 +0800

    feat: return all two-sum pairs

--- two-sum.py
+++ two-sum.py
@@ -1,1 +1,9 @@
-print("hello")
+def two_sum(nums, target):
+    result = {}
+    for i, num in enumerate(nums):
+        com = target - num
+        if com in result:
+            return [result[com],i]
+        result[num] = i
+    return None
+print (two_sum([1, 2, 7, 8, 11, 15], 9))
