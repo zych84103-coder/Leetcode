37cffc5cf00c80981fe909af541493b335e0cce5
zych84103-coder <zych84103@gmail.com>
2026-08-26 14:01:36 +0800

    feat: 33_search-in-rotated-sorted-array.py

---
+++ 33_search-in-rotated-sorted-array.py
@@ -0,0 +1,25 @@
+from typing import List
+
+class Solution():
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums)-1
+        while left <= right:
+            mid = (left + right)//2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] < nums[right]: #先一边有序
+                if nums[mid] < target <= nums[right]:    #确定在一边
+                    left = mid + 1
+                else:
+                    right = mid -1     #不在这一边
+            else:
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+        return -1
+
+s = Solution()
+print (s.search([4,5,6,7,0,1,2], 0))    #4
+print (s.search([4,5,6,7,0,1,2], 3))    #-1
+print (s.search([1], 0))                #-1
